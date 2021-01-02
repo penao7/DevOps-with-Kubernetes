9ade696466e10c465a3191eb9d4c2eac2b900926
peon <penonized@gmail.com>
2021-01-02 12:57:08 +0200

    fixed initial ping fetch

--- part1/tasks/pingpong-app/app.js
+++ part1/tasks/pingpong-app/app.js
@@ -7,13 +7,27 @@ const path = require('path');
 const directory = path.join('/', 'usr', 'app');
 const filePath = path.join(directory, 'pingpong.txt');
 
-let pong = 0;
+let pong = '';
 
 const output = () => {
   return `${pong}`;
 };
 
+const getPingPong = async () => {
+  try {
+    const pingpong = fs.readFile(filePath, 'utf-8');
+    console.log('pingpongs fetched from pongpong.txt');
+    return pingpong;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const incrementOnGet = async () => {
+  if (pong === '') {
+    pong = await getPingPong();
+  };
+
   pong++;
 
   try {
