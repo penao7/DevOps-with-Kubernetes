664bb2dddd1dcceaaa89eafeb5c005a1218abf2d
penao7 <56816663+penao7@users.noreply.github.com>
2021-01-26 16:32:22 +0200

    Delete deploy.sh

--- todo-app/todo-app-broadcaster/deploy.sh
+++
@@ -1,5 +0,0 @@
-kubectl delete deployments.apps todo-broadcaster
-docker rmi penao7/todo-broadcaster
-docker build -t penao7/todo-broadcaster .
-docker push penao7/todo-broadcaster
-kubectl apply -f manifests/deployment.yaml
